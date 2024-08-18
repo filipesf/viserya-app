d70445a4b0006c0644fd47411acb3fcddb9560a3
Filipe Fernandes <me@filipesf.com>
2024-08-18 20:50:14 +0100

    add startsession command, update button styles

--- app/api/bot/interactions/route.ts
+++ app/api/bot/interactions/route.ts
@@ -1,12 +1,12 @@
 import { InteractionType, APIInteractionResponse } from 'discord.js';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { DISCORD_KEY } from '@viserya/config/constants';
 import getCommands from '@viserya/utils/getCommands';
 import { verifyInteractionRequest } from '@viserya/utils/verifyDiscordRequest';
 
-export async function POST(req: Request) {
+export async function POST(request: NextRequest) {
   try {
-    const verifyRes = await verifyInteractionRequest(req, DISCORD_KEY);
+    const verifyRes = await verifyInteractionRequest(request, DISCORD_KEY);
 
     if (!verifyRes.isValid || !verifyRes.interaction) {
       return new NextResponse('Invalid request', { status: 401 });
@@ -19,6 +19,10 @@ export async function POST(req: Request) {
       return NextResponse.json({ type: 1 });
     }
 
+    console.log('=== INTERACTION ===', interaction);
+
+    console.log('=== NextResponse 1 ===', NextResponse);
+
     // get all commands
     const allCommands = await getCommands();
 
@@ -29,7 +33,12 @@ export async function POST(req: Request) {
       reply = await allCommands[commandName].execute(interaction);
     }
 
+    console.log('=== NextResponse 2 ===', NextResponse);
+
     if (!reply) throw new Error();
+
+    console.log('=== NextResponse 3 ===', NextResponse);
+    console.log('=== reply ===', reply);
     return NextResponse.json(reply);
   } catch (error: any) {
     console.log(error);
