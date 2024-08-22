7eca5acbcc12548f01fc53df6ec2d543f2423eb0
Filipe Fernandes <me@filipesf.com>
2024-08-22 22:36:44 +0100

    update session messages endpoint

--- app/api/sessions/[channelId]/messages/route.ts
+++ app/api/sessions/[channelId]/messages/route.ts
@@ -30,38 +30,41 @@ export async function POST(
   { params: { channelId } }: SessionsParams,
 ) {
   const { searchParams } = new URL(request.url);
-  const messageString = searchParams.get('message');
   const threadId = searchParams.get('threadId');
   const userId = searchParams.get('userId');
+  const role = searchParams.get('role');
+  const text = searchParams.get('text');
   const type = searchParams.get('type');
 
-  let message: ChatMessage;
+  console.log('🐞 POST sessions/[channelId]/messages', request);
 
-  try {
-    console.log('✍️ PARSING MESSAGE');
+  // let message: ChatMessage;
 
-    if (messageString) {
-      message = JSON.parse(messageString) as ChatMessage;
-      console.log('🆗 MESSAGE PARSED');
-    } else {
-      return NextResponse.json(
-        { error: '💀 Message parameter is missing.' },
-        { status: 400 },
-      );
-    }
-  } catch (error) {
-    return NextResponse.json(
-      { error: '💀 Invalid message format' },
-      { status: 400 },
-    );
-  }
+  // try {
+  //   console.log('✍️ PARSING MESSAGE');
+
+  //   if (messageString) {
+  //     message = JSON.parse(messageString) as ChatMessage;
+  //     console.log('🆗 MESSAGE PARSED');
+  //   } else {
+  //     return NextResponse.json(
+  //       { error: '💀 Message parameter is missing.' },
+  //       { status: 400 },
+  //     );
+  //   }
+  // } catch (error) {
+  //   return NextResponse.json(
+  //     { error: '💀 Invalid message format' },
+  //     { status: 400 },
+  //   );
+  // }
 
   try {
     console.log('📝 STORING MESSAGE');
 
     const result = await sql`
       INSERT INTO messages (channel_id, thread_id, user_id, role, text, type)
-      VALUES (${channelId}, ${threadId}, ${userId}, ${message.role}, ${message.text}, ${type});
+      VALUES (${channelId}, ${threadId}, ${userId}, ${role}, ${text}, ${type});
     `;
 
     console.log('✅ MESSAGE STORED', result);
