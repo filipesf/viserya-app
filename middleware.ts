1d4e25a1e1203c3e6da8fe29968bab085bd8f71e
Filipe Fernandes <me@filipesf.com>
2024-09-18 07:41:10 +0100

    update middleware to handle, CORS and OPTIONS Requests

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,23 @@ import { NextRequest, NextResponse } from 'next/server';
 import { AUTHORIZATION_KEY } from '@viserya/config/constants';
 
 export default async function middleware(request: NextRequest) {
-  // List of URL paths to exclude from the authorization check
+  if (request.method === 'OPTIONS') {
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, AUTHORIZATION_KEY',
+      },
+    });
+  }
+
   const exceptionPaths = ['/api/bot/interactions'];
 
-  // Check if the current request URL matches any of the exception paths
   if (
     exceptionPaths.some((path) => request.nextUrl.pathname.startsWith(path))
   ) {
-    // If the request matches an exception, skip the authorization check
     return NextResponse.next();
   }
 
