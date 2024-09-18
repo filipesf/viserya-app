1836ac1ae451899220967577a1b6d6508c44d273
Filipe Fernandes <me@filipesf.com>
2024-09-18 13:28:31 +0100

    🐞 debuggin assistant threads api

--- app/api/assistants/threads/[threadId]/messages/route.ts
+++ app/api/assistants/threads/[threadId]/messages/route.ts
@@ -1,15 +1,5 @@
 import { NextRequest } from 'next/server';
 import { assistantIds } from '@viserya/config/assistants';
-import {
-  ASSISTANT_ID,
-  AUTHORIZATION_KEY,
-  CHARSHEET_API,
-  DISCORD_BOT_TOKEN,
-  DISCORD_KEY,
-  LOCAL_STORAGE_THEME,
-  NEXT_PUBLIC_DISCORD_APP_ID,
-  OPENAI_API_KEY,
-} from '@viserya/config/constants';
 import { openai } from '@viserya/config/openai';
 import { MessagesRecord } from '@viserya/types';
 import { AssistantMessageParams } from '@viserya/types/openai';
@@ -39,44 +29,28 @@ export async function POST(
     return { text, type: 'text' };
   });
 
-  console.log('🐛 Filtered content:', content);
-  console.log('🐛 assistantIds', assistantIds);
-  console.log('🐛 ENV', {
-    AUTHORIZATION_KEY,
-    OPENAI_API_KEY,
-    ASSISTANT_ID,
-    NEXT_PUBLIC_DISCORD_APP_ID,
-    DISCORD_BOT_TOKEN,
-    DISCORD_KEY,
-    CHARSHEET_API,
-    LOCAL_STORAGE_THEME,
-  });
-
   await openai.beta.threads.messages.create(threadId, {
     role: 'user',
     content: content,
   });
 
-  let run = await openai.beta.threads.runs.createAndPoll(threadId, {
+  // TODO: Refactor the bot to handle messages without streaming, implementing the code below
+  // let run = await openai.beta.threads.runs.createAndPoll(threadId, {
+  //   assistant_id: assistantIds.dm,
+  // });
+  //
+  // if (run.status === 'completed') {
+  //   const messages = await openai.beta.threads.messages.list(run.thread_id);
+  //   for (const message of messages.data.reverse()) {
+  //     console.log('🪲', run.status, message.content);
+  //   }
+  // } else {
+  //   console.log('🐞', run.status);
+  // }
+
+  const stream = openai.beta.threads.runs.stream(threadId, {
     assistant_id: assistantIds.dm,
   });
 
-  if (run.status === 'completed') {
-    const messages = await openai.beta.threads.messages.list(run.thread_id);
-    for (const message of messages.data.reverse()) {
-      console.log('🪲', run.status, message);
-    }
-  } else {
-    console.log('🐞', run.status);
-  }
-
-  // const stream = openai.beta.threads.runs.stream(threadId, {
-  //   assistant_id: assistantIds.dm,
-  // });
-
-  return new Response(null, {
-    status: 200,
-  });
-
-  // return new Response(stream.toReadableStream());
+  return new Response(stream.toReadableStream());
 }
